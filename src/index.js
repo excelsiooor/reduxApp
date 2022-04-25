f64dc71a6ac57df1dd2ff33424b4e540a5be5540
xomastyle- <homenich101@gmail.com>
2022-04-26 00:39:51 +0300

    add middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import {createStore, compose, applyMiddleware} from 'redux';
+import { spamFilter } from './redux/middleware';
 import {rootReducer} from './redux/rootReducer';
 import { Provider } from 'react-redux';
 import './index.css';
@@ -9,7 +10,8 @@ import App from './App';
 
 const store = createStore(rootReducer, compose(
   applyMiddleware(
-    thunk
+    thunk,
+    spamFilter
     ),
     window.__REDUX_DEVTOOLS_EXTENSION__
         ? window.__REDUX_DEVTOOLS_EXTENSION__()
